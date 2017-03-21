75987f510211d6eb228d6f9d6bd730cb1a4872fb
Sin Cheung Ko <scko823@gmail.com>
2017-03-20 22:05:16 -0700

    preliminary template set up

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 	"strconv"
@@ -10,14 +11,30 @@ import (
 
 var mockMessages []string
 var counter = 3
+var tmpl *template.Template
 
 func init() {
+	var err error
 	mockMessages = []string{"message1", "message2"}
+	tmpl, err = template.ParseGlob("templates/*")
+	if err != nil {
+		// log.Fatalf("there is an error when trying to parse templates. Err: %v\n", err)
+	}
 }
 
 func main() {
+	http.HandleFunc("/", index)
 	http.HandleFunc("/messages", messages)
 	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "index.gohtml", struct {
+		Messages []string
+	}{
+		mockMessages,
+	})
+	return
 }
 
 func messages(w http.ResponseWriter, r *http.Request) {
