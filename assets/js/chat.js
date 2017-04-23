fcd6ba5336cbfa3f9b27d9e47d58ade92d4a8d2e
Sin Cheung Ko <scko823@gmail.com>
2017-04-22 17:10:30 -0700

    using vue js for the list of messages

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,8 +1,39 @@
 // this is for everything about the chat room
-const newMessage = new Vue({
-  el: '#send-message',
-  data: {
-    newMessageText: ''
+conn = new WebSocket("ws://" + document.location.host + "/ws?room=" + (getParameterByName('room')||'main') )
+
+const messageList = Vue.component('message-list', {
+  template: `<div id="message">
+    <ul id="message-list">
+      <li v-for="msg in messages">
+        {{msg.message}}
+      </li>
+    </ul>
+  </div>`,
+  data: function () {
+    return {
+      messages: []
+    }
+  },
+  mounted: function () {
+    const self = this
+    conn.onmessage = function(evt){
+      let message = JSON.parse(event.data)
+      self.messages = self.messages.concat([message])
+    }
+  }
+})
+
+
+Vue.component('send-message',{
+  template:`<div id="send-message">
+    <form id="sumbit" class="message" @submit.prevent="submitNewMessage">
+      <input v-model="newMessageText" />
+    </form>
+  </div>`,
+  data: function () {
+    return {
+      newMessageText: ''
+    }
   },
   methods: {
     submitNewMessage: function () {
@@ -11,4 +42,8 @@ const newMessage = new Vue({
       this.newMessageText = ''
     }
   }
+})
+
+new Vue({
+  el:'#app'
 })
